ffd861c4c444250a8ea2e2f67de46588ec6081b0
John Lavery <121765789+jlavery249@users.noreply.github.com>
2023-01-02 11:20:35 -0500

    Update Password-manager_v1.ps1
    Added comments and header

--- Password-manager_v1.ps1
+++ Password-manager_v1.ps1
@@ -1,3 +1,15 @@
+############################################################################################################
+#  Password Manager v1                                                                                     #
+#  Created by John Lavery                                                                                  #
+#  Year of 2019                                                                                            #
+#  Written in Powershell                                                                                   #
+#  Description:                                                                                            #
+#  Takes user input such as site name, username, password you want to use and stores it in a csv file      #
+#  based on specified path output                                                                          #
+############################################################################################################
+
+## Declare functions
+
 function ConvertToExcel
 {
     $data = (Get-Content '<path to>passwordfile.txt') -replace ':', '=' |
@@ -15,6 +27,8 @@ function ConvertToPlainText
     $result 
 }
 
+## for user input about account info
+
 $UN = Read-Host "Please enter account username"
 $PW = Read-Host "Enter Password" -AsSecureString
 $site = Read-Host "Enter website this is used for"
@@ -26,5 +40,6 @@ Write-Output "Site: $site" | Out-File "<path to>passwordfile.txt" -Append -Encod
 Write-Output "Username: $UN" | Out-File "<path to>passwordfile.txt" -Append -Encoding unicode
 Write-Output "Password: $plain" | Out-File "<path to>passwordfile"
 
+## puts through converttoexcel function and pushes out password file csv to specified path
 Converttoexcel
 remove-item -path "<path to>passwordfile.txt" -Recurse 
